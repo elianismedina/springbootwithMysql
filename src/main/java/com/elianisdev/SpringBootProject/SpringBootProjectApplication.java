aaed06563c4214b86ef64508f4986de50acb71df
elianismedina <elianismedina05@outlook.com>
2024-09-24 23:46:01 -0500

    second commit

---
+++ src/main/java/com/elianisdev/SpringBootProject/SpringBootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.elianisdev.SpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+
+}
